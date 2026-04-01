61fc3234f01c8d3892f2b6c7ec43fd286005abd4
Stephane Mfuni <stephuni35@gmail.com>
2026-04-01 15:31:05 +0100

    fixed design

--- components/configurations/RoomListCard.tsx
+++ components/configurations/RoomListCard.tsx
@@ -16,22 +16,22 @@ const RoomListCard = ({ room, onDelete, onEdit }: Props) => {
   return (
     <div
       className="group flex items-center justify-between p-4 rounded-2xl border border-gray-200
-     bg-white shadow-sm hover:shadow-md transition-all duration-200"
+     bg-white shadow-sm hover:shadow-md transition-all duration-200 flex-wrap gap-y-2"
     >
       {/* LEFT */}
       <div className="flex flex-col">
-        <span className="text-base font-semibold text-gray-800">
+        <span className="text-base font-semibold text-gray-800 capitalize">
           {room.name}
         </span>
         <span className="text-xs text-gray-400">ID: {room.id}</span>
       </div>
 
       {/* RIGHT ACTIONS */}
-      <div className="flex items-center gap-2 ">
+      <div className="flex items-center gap-2 ml-auto">
         {/* EDIT */}
         <button
           onClick={() => onEdit(room)}
-          className="px-3 py-1.5 text-sm rounded-lg border border-gray-200 hover:bg-gray-100 transition"
+          className="px-3 py-1.5 text-xs xl:text-sm rounded-lg border border-gray-200 hover:bg-gray-100 transition"
         >
           Modifier
         </button>
@@ -39,7 +39,7 @@ const RoomListCard = ({ room, onDelete, onEdit }: Props) => {
         {/* DELETE */}
         <button
           onClick={() => onDelete(room.id)}
-          className="px-3 py-1.5 text-sm rounded-lg bg-red-500 text-white hover:bg-red-600 transition"
+          className="px-3 py-1.5 text-xs xl:text-sm rounded-lg bg-red-500 text-white hover:bg-red-600 transition"
         >
           Supprimer
         </button>
